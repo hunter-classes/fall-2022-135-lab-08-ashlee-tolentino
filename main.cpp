0acc4019cd4209d8f1168e45e448d696a7c5c756
Ashlee Tolentino <104875387+ashlee-tolentino@users.noreply.github.com>
2022-10-26 10:52:50 -0400

    added invert-half example

--- main.cpp
+++ main.cpp
@@ -4,16 +4,18 @@ Course: CSCI-135
 Instructor: Michael Zamansky
 Assignment: Lab8
 
-This program...
+This program performs changes to the colors/features of a pgm image file.
 */
 
 #include <iostream>
 #include <string>
 #include "imageio.h"
 #include "invert.h"
+#include "invert-half.h"
 
 int main()
 {
+  // Task A
   std::string input = "image1.pgm";
   int img[MAX_H][MAX_W];
   int h, w;
@@ -29,6 +31,20 @@ int main()
   }
   
   writeImage("taskA.pgm", out, h, w);
+
+  // Task B
+  readImage(input, img, h, w);
+  invertHalf(input, img, h, w);
+
+  for(int row = 0; row < h; row++) {
+    for(int col = 0; col < w; col++) {
+      out[row][col] = img[row][col];
+    }
+  }
+  
+  writeImage("taskB.pgm", out, h, w);
+
+  
 
   return 0;
 }
