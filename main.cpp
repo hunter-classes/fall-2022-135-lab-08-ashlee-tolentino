d145b13d8571273a826b17bcefa4c9ea25d46f5b
Ashlee Tolentino <104875387+ashlee-tolentino@users.noreply.github.com>
2022-10-31 23:41:05 -0400

    added pixelate example

--- main.cpp
+++ main.cpp
@@ -14,6 +14,8 @@ This program performs changes to the colors/features of a pgm image file.
 #include "invert-half.h"
 #include "box.h"
 #include "frame.h"
+//#include "scale.h"
+#include "pixelate.h"
 
 int main()
 {
@@ -66,7 +68,30 @@ int main()
   }
   writeImage("taskD.pgm", out, h, w);
 
-  //
+  /*
+  // Task E
+  readImage("inImage.pgm", img, h, w);
+  int* result[h][w];
+  result = scale("inImage.pgm", img, h, w);
+
+  for(int row = 0; row < h; row++) {
+    for(int col = 0; col < w; col++) {
+      out[row][col] = result*[row][col];
+    }
+  }
+  writeImage("taskE.pgm", out, h, w);
+  */
+
+  // Task F
+  readImage(input, img, h, w);
+  pixelate(input, img, h, w);
+
+  for(int row = 0; row < h; row++) {
+    for(int col = 0; col < w; col++) {
+      out[row][col] = img[row][col];
+    }
+  }
+  writeImage("taskF.pgm", out, h, w);
 
   return 0;
 }
